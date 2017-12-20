20caa5065b7f298d02f2a2f851c430dbb6c68253
Amit Rudra <amit.rudra@ericsson.com>
2017-12-20 13:05:09 +0530

    SASS Starter Pack.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "."  
+    });
+
+    gulp.watch(['scss/*.scss'], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
